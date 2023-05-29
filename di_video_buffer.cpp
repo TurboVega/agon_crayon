e5d20c3eeda992c44c518acca1fad438438fa600
Curtis Whitley <cwhitley@pesa.com>
2023-05-28 21:19:22 -0400

    first tile display is unstable

--- di_video_buffer.cpp
+++ di_video_buffer.cpp
@@ -54,7 +54,7 @@ uint8_t sdcolor[10] = {
   SYNCS_OFF | MASK_RGB(1,2,3)
 };
 
-#define STAR_PADDING 200
+/*#define STAR_PADDING 200
 #define NUM_STARS (ACT_LINES+STAR_PADDING)
 DiSetPixel g_stars[NUM_STARS];
 
@@ -64,7 +64,8 @@ DiDiagonalRightLine g_diamond_ne(CENTER_X, CENTER_Y-HALF_DIAMOND_SIZE, HALF_DIAM
 DiDiagonalRightLine g_diamond_sw(CENTER_X-HALF_DIAMOND_SIZE, CENTER_Y, HALF_DIAMOND_SIZE,  MASK_RGB(2,3,1));
 DiDiagonalLeftLine g_diamond_nw(CENTER_X, CENTER_Y-HALF_DIAMOND_SIZE, HALF_DIAMOND_SIZE,  MASK_RGB(2,3,1));
 DiDiagonalLeftLine g_diamond_se(CENTER_X+HALF_DIAMOND_SIZE-1, CENTER_Y, HALF_DIAMOND_SIZE,  MASK_RGB(2,3,1));
-DiOpaqueBitmap* gp_opaque_bitmap0;
+*/
+/*DiOpaqueBitmap* gp_opaque_bitmap0;
 DiOpaqueBitmap* gp_opaque_bitmap1;
 DiOpaqueBitmap* gp_opaque_bitmap2;
 DiOpaqueBitmap* gp_opaque_bitmap3;
@@ -159,8 +160,14 @@ static const char* digit_data =
 "    *"
 "*   *"
 " *** ";
+*/
 
-void breakdown_value(uint32_t value, DiOpaqueBitmap** digits) {
+#define TILES_ACROSS 20
+#define TILES_DOWN   20
+
+DiOpaqueBitmap* tile;
+
+/*void breakdown_value(uint32_t value, DiOpaqueBitmap** digits) {
   uint32_t d5 = value/100000; value=value%100000;
   uint32_t d4 = value/10000; value=value%10000;
   uint32_t d3 = value/1000; value=value%1000;
@@ -174,10 +181,10 @@ void breakdown_value(uint32_t value, DiOpaqueBitmap** digits) {
   digits[2] = gp_digit_bitmap[d2];
   digits[1] = gp_digit_bitmap[d1];
   digits[0] = gp_digit_bitmap[d0];
-}
+}*/
 
 void init_stars() {
-  srand(42);
+/*  srand(42);
   for (int i = 0; i < NUM_STARS; i++) {
     int32_t x = rand() % (ACT_PIXELS * 3 / 2) - (ACT_PIXELS / 4);
     int8_t c = (int8_t)(rand() % 63 + 1);
@@ -246,7 +253,15 @@ void init_stars() {
   breakdown_value(heap_caps_get_largest_free_block(MALLOC_CAP_32BIT|MALLOC_CAP_8BIT|MALLOC_CAP_INTERNAL), gp_value_bitmap[1]);
   breakdown_value(heap_caps_get_free_size(MALLOC_CAP_INTERNAL), gp_value_bitmap[2]);
   breakdown_value(heap_caps_get_largest_free_block(MALLOC_CAP_INTERNAL), gp_value_bitmap[3]);
+*/
 
+  tile = new(40,30) DiOpaqueBitmap(40,30);
+  tile->clear();
+  for (int32_t y=0;y<30;y++) {
+    for (int32_t x=0;x<40;x++) {
+      tile->set_pixel(x, y, gtest_bitmapData[y*64+x]);
+    }
+  }
 }
 
 void DiVideoScanLine::init_to_black() {
@@ -263,7 +278,7 @@ void DiVideoScanLine::init_for_vsync() {
   memset(m_hbp, (HSYNC_OFF|VSYNC_ON), HBP_PIXELS);
 }
 
-void show_value(DiOpaqueBitmap** digits, uint32_t y, DiPaintParams *params) {
+/*void show_value(DiOpaqueBitmap** digits, uint32_t y, DiPaintParams *params) {
   digits[5]->set_position(700,y);
   digits[5]->paint(params);
   digits[4]->set_position(706,y);
@@ -276,15 +291,15 @@ void show_value(DiOpaqueBitmap** digits, uint32_t y, DiPaintParams *params) {
   digits[1]->paint(params);
   digits[0]->set_position(730,y);
   digits[0]->paint(params);
-}
+}*/
 
 void IRAM_ATTR DiVideoScanLine::paint(DiPaintParams *params) {
   params->m_line32 = (uint32_t*)(m_act);
   params->m_line8 = (uint8_t*)(m_act);
   params->m_scrolled_y = params->m_line_index + params->m_vert_scroll;
 
-  memset(params->m_line8, SYNCS_OFF, ACT_PIXELS);
-
+//  memset(params->m_line8, SYNCS_OFF, ACT_PIXELS);
+/*
   if (params->m_scrolled_y >= 500) {
     show_value(gp_value_bitmap[0], 500, params);
     show_value(gp_value_bitmap[1], 510, params);
@@ -304,7 +319,7 @@ void IRAM_ATTR DiVideoScanLine::paint(DiPaintParams *params) {
   g_diamond_nw.paint(params);
   g_diamond_se.paint(params);
 
-/*
+
   // Draw small diamond shapes somewhere.
   for (int d = 0; d<10; d++) {
     uint32_t sdtop = sdy[d] - HALF_SMALL_DIAMOND_SIZE;
@@ -332,7 +347,7 @@ void IRAM_ATTR DiVideoScanLine::paint(DiPaintParams *params) {
       params->m_line8[FIX_INDEX(sdx[d]+1+sd_offset)] = diamond;
     }
   }
-*/
+
   // Draw a bitmap
   DiPaintParams p2 = *params;
   p2.m_horiz_scroll = 0;
@@ -351,6 +366,15 @@ void IRAM_ATTR DiVideoScanLine::paint(DiPaintParams *params) {
 
   //gp_opaque_bitmap->paint(&p2);
  // gp_masked_bitmap->paint(params);
+ */
+
+  int32_t r = params->m_scrolled_y / 30;
+  if (r >= 0 && r < TILES_DOWN) {
+    for (int32_t c = 0; c < TILES_ACROSS; c++) {
+      tile->set_position(c * 40, r * 30);
+      tile->paint(params);
+    }
+  }
 }
 
 void DiVideoBuffer::init_to_black() {
