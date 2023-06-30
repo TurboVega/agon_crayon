b91d3da0ffebc87b860eb4b5afb591ef150ecbad
Curtis Whitley <cwhitley@pesa.com>
2023-06-30 15:06:48 -0400

    Editing samples again

---
+++ samples/convert.bat
@@ -0,0 +1,23 @@
+c:image2agon.exe pct0.png
+c:image2agon.exe pct33.png
+c:image2agon.exe pct50.png
+c:image2agon.exe pct66.png
+c:image2agon.exe pct100.png
+
+c:image2agon.exe pct0_merged.png
+c:image2agon.exe pct33_merged.png
+c:image2agon.exe pct50_merged.png
+c:image2agon.exe pct66_merged.png
+c:image2agon.exe pct100_merged.png
+
+..\BinToHex.exe PCT0.BIN PCT0.h Pct0
+..\BinToHex.exe PCT33.BIN PCT33.h Pct33
+..\BinToHex.exe PCT50.BIN PCT50.h Pct50
+..\BinToHex.exe PCT66.BIN PCT66.h Pct66
+..\BinToHex.exe PCT100.BIN PCT100.h Pct100
+
+..\BinToHex.exe PCT0_MERGED.BIN PCT0_MERGED.h Pct0_Merged
+..\BinToHex.exe PCT33_MERGED.BIN PCT33_MERGED.h Pct33_Merged
+..\BinToHex.exe PCT50_MERGED.BIN PCT50_MERGED.h Pct50_Merged
+..\BinToHex.exe PCT66_MERGED.BIN PCT66_MERGED.h Pct66_Merged
+..\BinToHex.exe PCT100_MERGED.BIN PCT100_MERGED.h Pct100_Merged
